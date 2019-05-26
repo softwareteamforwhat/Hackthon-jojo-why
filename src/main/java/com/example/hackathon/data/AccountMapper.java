de859cf92a081a5ac224520ecb4a3dd95d5a5482
AlexHSL <2825883930@qq.com>
2019-05-27 01:09:09 +0800

    110

--- src/main/java/com/example/hackathon/data/AccountMapper.java
+++ src/main/java/com/example/hackathon/data/AccountMapper.java
@@ -2,7 +2,8 @@ package com.example.hackathon.data;
 
 import com.example.hackathon.po.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Mapper
 public interface AccountMapper {
 
@@ -11,12 +12,12 @@ public interface AccountMapper {
      * @param username,password
      * @return userId
      */
-    int createNewAccount(@Param("username") String username, @Param("password") String password);
+    int createNewAccount(@RequestParam("username") String username, @RequestParam("password") String password);
 
     /**
      * 以用户名搜索用户
      * @param username
      * @return User
      */
-    User getAccountByName(@Param("username") String username);
+    User getAccountByName(@RequestParam("username") String username);
 }
