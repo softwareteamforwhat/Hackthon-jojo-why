c3a390323e89d249101325a24966e2f175d93c44
AlexHSL <2825883930@qq.com>
2019-05-27 09:00:29 +0800

    200

--- src/main/java/com/example/hackathon/Controller/WorkGroupController.java
+++ src/main/java/com/example/hackathon/Controller/WorkGroupController.java
@@ -6,10 +6,7 @@ import com.example.hackathon.vo.ResponseVO;
 import com.example.hackathon.vo.UserForm;
 import com.example.hackathon.vo.WorkGroupForm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WorkGroupController {
@@ -17,25 +14,25 @@ public class WorkGroupController {
     private WorkGroupService workGroupService;
 
     @RequestMapping(value = "/work_group/add", method = RequestMethod.POST)
-    public ResponseVO addWorkGroup(@RequestBody WorkGroupForm workGroupForm){
+    public ResponseVO addWorkGroup(@RequestParam("workGroupForm") WorkGroupForm workGroupForm){
         return workGroupService.addWorkGroup(workGroupForm);
     }
     @RequestMapping(value = "/work_group/all", method = RequestMethod.POST)
-    public ResponseVO getWorkGroupById(@RequestBody int id){
+    public ResponseVO getWorkGroupById(@RequestParam("userId") int id){
         return workGroupService.getWorkGroupById(id);
     }
 
     @RequestMapping(value = "/work_group/off",method = RequestMethod.POST)
-    public ResponseVO removeGroup(@RequestBody int id){
+    public ResponseVO removeGroup(@RequestParam("groupId") int id){
         return workGroupService.removeWorkGroup(id);
     }
 
     @RequestMapping(value = "/work_group/update",method = RequestMethod.POST)
-    public ResponseVO updateGroup(@RequestBody WorkGroupForm  workGroupForm){
+    public ResponseVO updateGroup(@RequestParam("workGroupForm") WorkGroupForm  workGroupForm){
         return workGroupService.updateWorkGroup(workGroupForm);
     }
     @RequestMapping(value = "/work_group/add_member",method = RequestMethod.POST)
-    public ResponseVO addMember(@RequestBody UserForm userForm,@RequestBody WorkGroupForm  workGroupForm){
+    public ResponseVO addMember(@RequestParam("userForm") UserForm userForm,@RequestParam("workGroupForm") WorkGroupForm  workGroupForm){
         return workGroupService.addMember(userForm,workGroupForm);
     }
 }
